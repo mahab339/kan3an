afb6443b6072ff920dd50256b806fad6862b8f6d
Mohab Hassan <mohabh@localhost.localdomain>
2015-07-11 01:13:59 +0300

    Add bash script that make activity runnable.
    The script will modify AndroidManifest.xml file by adding android:exported="true" attribute to the activity element and then undo this change after the building process.
    The reason for adding this is becuase to not encounter "java.lang.SecurityException Permission Denial" while starting the activity.

--- roya.sh
+++ roya.sh
@@ -1,6 +1,29 @@
 #!/usr/bin/bash
+
+make_activity_runnable()
+{
+	echo "Adding android:exported="true" to activity $1 in AndroidManifest.xml"
+	#using $(MANIFEST_PATH), instead of the explisit path, raise permesion denide error
+	MANIFEST_PATH=./app/src/main/AndroidManifest.xml
+	# lines from <activity > starting from android:name until >
+	# assuming the first line after <activity is android:name="", L contains all attributes of <activity >
+	if [[ "$L" != *"android:exported=\"true\""* ]]; then
+		#add exported="true" after android:name=""
+			sed -i.copy 's/android:name="com.azhalha.androidapp.'$1'"/android:name="com.azhalha.androidapp.'$1'"\n\tandroid:exported="true"/' ./app/src/main/AndroidManifest.xml
+		  echo "done modifying"
+	fi
+}
+
+invert_manifest_modification()
+{
+	echo "invert modificating done to AndroidManifest.xml"
+	mv $MANIFEST_PATH".copy" $MANIFEST_PATH
+}
+
 if [ -f ./gradlew ]; then
+	make_activity_runnable $1
 	./gradlew assembleDebug
+	invert_manifest_modification
 	adb install -r ./app/build/outputs/apk/app-debug.apk
 	adb shell am start -n com.azhalha.androidapp/com.azhalha.androidapp.$1
 else
