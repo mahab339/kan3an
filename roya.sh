1879d2c001741c8fd38e2efc29b33597a77e843b
Mohab Hassan <mohabh@localhost.localdomain>
2015-07-17 14:11:36 +0300

    Add ACTIVITY_ATTR variable to make_activity_runnable()
    this will hold activity attributes to be checked if it contains android:exported="true" or not. If so, the function will skip adding this attribute.

--- roya.sh
+++ roya.sh
@@ -4,22 +4,30 @@ PACKAGE_NAME="com.azhalha.androidapp"
 
 make_activity_runnable()
 {
-	echo "Adding android:exported="true" to activity $1 in AndroidManifest.xml"
+	echo "Adding android:exported=\"true\" to activity $1 in AndroidManifest.xml"
 	#using $(MANIFEST_PATH), instead of the explisit path, raise permesion denide error
 	MANIFEST_PATH=./app/src/main/AndroidManifest.xml
 	# lines from <activity > starting from android:name until >
-	# assuming the first line after <activity is android:name="", L contains all attributes of <activity >
-	if [[ "$L" != *"android:exported=\"true\""* ]]; then
+	# assuming the first line after <activity is android:name="", ACTIVITY_ATTRS contains all attributes of <activity >
+	ACTIVITY_ATTRS=$(sed -n '/android:name="'$PACKAGE_NAME'.'$1'"/,/>/p' ./app/src/main/AndroidManifest.xml)
+	if [[ "$ACTIVITY_ATTRS" != *"android:exported=\"true\""* ]]; then
 		#add exported="true" after android:name=""
-			sed -i.copy 's/android:name="'$PACKAGE_NAME'.'$1'"/android:name="'$PACKAGE_NAME'.'$1'"\n\tandroid:exported="true"/' ./app/src/main/AndroidManifest.xml
-		  echo "done modifying"
+		sed -i.copy 's/android:name="'$PACKAGE_NAME'.'$1'"/android:name="'$PACKAGE_NAME'.'$1'"\n\tandroid:exported="true"/' ./app/src/main/AndroidManifest.xml
+		echo "Done modifying..."
+	else
+		echo "Nothing to modify..."
 	fi
 }
 
+# replace the AndroidManifest.xml.copy, which is the manifest file before
+# applying the modifications, with the modified manifest.
+# this method is usally called afer building the application
 invert_manifest_modification()
 {
-	echo "invert modificating done to AndroidManifest.xml"
-	mv $MANIFEST_PATH".copy" $MANIFEST_PATH
+	if [ -a $MANIFEST_PATH".copy" ]; then
+		echo "Inverting modificating done to AndroidManifest.xml"
+		mv $MANIFEST_PATH".copy" $MANIFEST_PATH
+	fi
 }
 
 if [ -f ./gradlew ]; then
